65979f2725a12c220b7c7a5bea506a829fb4eb60
Junichiro Ishibashi <jay.small.business@outlook.com>
2025-12-04 22:32:08 +0900

    update with MYSQL_DATABASE

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ async function getDB() {
     port: process.env.MYSQL_PORT,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
-    database: "railway",
+    database: process.env.MYSQL_DATABASE,
   });
 }
 
