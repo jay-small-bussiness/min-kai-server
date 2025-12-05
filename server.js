54fc86734903cbfab18c9fb5dfeb313a14587855
Junichiro Ishibashi <jay.small.business@outlook.com>
2025-12-05 09:59:25 +0900

    update with hirahira

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ async function getDB() {
   return mysql.createConnection({
     host: process.env.MYSQL_HOST,
     port: process.env.MYSQL_PORT,
-    user: "root",
-    password: "KFHgduBLQzgESYpgxDMBpqgMaVZOfzqB",
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
     database: "railway",
   });
 }
